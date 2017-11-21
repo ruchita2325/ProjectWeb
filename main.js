022af0fdca06722c84621d3bf38e2a35794d995b
ruchita2325 <ruchu2325@gmail.com>
2017-11-21 20:40:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+    
+    $("#div2").hide();
+});
+$(document).ready(function(){
+    $("#b1").click(function(){
+        $("#div2").show();
+        $("#div3").hide();
+    });
+});
+$(document).ready(function(){
+    
+    $("#div3").hide();
+});
+$(document).ready(function(){
+    $("#b3").click(function(){
+        $("#div3").show();
+        $("#div2").hide();
+    });
+});
+var add=()=>
+{
+    let i=document.getElementById("id").value;
+    let n=document.getElementById("name").value;
+    let c=document.getElementById("course").value;
+    let table=document.getElementById("table");
+    let tr=document.createElement("tr");
+    let t=" ";
+    t+="<td><input class='cb' type='checkbox' /></td>"+"<td>"+i+"</td>"+"<td>"+n+"</td>"+"<td>"+c+"</td>";
+    tr.innerHTML=t;
+    table.appendChild(tr);
+}
+function sub()
+{
+    add();
+}
+var d=()=>
+{
+    let table=document.getElementById("table"); 
+    let cb=table.getElementsByClassName("cb");
+    for(let i=0;i<cb.length;i++)
+    {
+        if(cb[i].checked)
+        {
+            table.deleteRow(i+1);
+            i--;
+            cb.length--;
+        }
+    } 
+}
+function del()
+{
+    d();
+}
+var ed=()=>
+{
+    let ii=document.getElementById("e_id").value;
+    let n=document.getElementById("e_name").value;
+    let c=document.getElementById("e_course").value;
+    let table=document.getElementById("table");
+    let cb=table.getElementsByClassName("cb");
+    for(var i=0;i<cb.length;i++)
+    {
+        if(cb[i].checked)
+        {
+            var tr=document.createElement("tr");
+            var t=" ";
+            t+="<td><input class='cb' type='checkbox' /></td>"+"<td>"+ii+"</td>"+"<td>"+n+"</td>"+"<td>"+c+"</td>";
+            tr.innerHTML=t;
+            
+            table.replaceChild(tr,table.childNodes[i+1]); 
+        }
+    }
+}
+function edit()
+{
+    ed();
+}
